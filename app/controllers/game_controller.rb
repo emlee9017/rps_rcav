2e87a8627c513324e96f318b972437a101bbdc89
emlee9017 <elee18@chicagobooth.edu>
2015-10-23 00:32:22 -0500

    Week 4 HW

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,16 +1,51 @@
 class  GameController < ApplicationController
   def rock
     @computer_move = ["rock","paper","scissors"].sample
+    
+    if @computer_move=="rock"
+        @result ="tied"
+        @button_type = "warning"
+    elsif @computer_move=="paper"
+        @result ="lost!"
+        @button_type="danger"
+    elsif @computer_move=="scissors"
+        @result ="win!"
+        @button_type="success"
+    end
 
     render("rock.html.erb")
   end
+
   def scissors
     @computer_move = ["rock","paper","scissors"].sample
+    
+    if @computer_move=="scissors"
+        @result ="tied"
+        @button_type = "warning"
+    elsif @computer_move=="rock"
+        @result ="lost!"
+        @button_type="danger"
+    elsif @computer_move=="paper"
+        @result ="win!"
+        @button_type="success"
+    end
 
     render("scissors.html.erb")
   end
+
   def paper
     @computer_move = ["rock","paper","scissors"].sample
+    
+    if @computer_move=="paper"
+        @result ="tied"
+        @button_type = "warning"
+    elsif @computer_move=="scissors"
+        @result ="lost!"
+        @button_type="danger"
+    elsif @computer_move=="rock"
+        @result ="win!"
+        @button_type="success"
+    end
 
     render("paper.html.erb")
   end
